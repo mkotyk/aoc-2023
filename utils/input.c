27be316fcdc0ffaa90b30c16abe9bd05927dfe4d
Mark Kotyk <mkotyk@gmail.com>
2023-12-01 11:26:16 -0500

    Day 1 - Building up a toolbox of reusable string utils, testing macros, and timing utils

--- utils/input.c
+++ utils/input.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "input.h"
 
-void read_input(const char *filename, cb_on_line on_line, cb_on_error on_error)
-{
+void read_input(const char *filename, cb_on_line on_line, cb_on_error on_error, void* ctx) {
     char *ptr, buffer[100];
     FILE *in;
+    int line_num = 1;
     if ((in = fopen(filename, "rt")) != NULL) {
-        while((ptr = fgets(buffer, sizeof(buffer) - 1, in)) != NULL)
-            on_line(ptr);
+        while((ptr = fgets(buffer, sizeof(buffer) - 1, in)) != NULL) {
+            on_line(ptr, line_num, ctx);
+            line_num++;
+        }
         fclose(in);
     } else {
-        on_error("Unable to open input file");
+        on_error("Unable to open input file", ctx);
     }
 }
 
-void split_line(const char *line, const char *delims, cb_on_token on_token)
-{
+void sample_input(const char *input, cb_on_line on_line, void* ctx) {
+    char *copy, *ptr, *start;
+    int line_num = 1;
+    if ((copy = strdup(input)) != NULL) {
+        start = copy;
+        while((ptr = strsep(&start, "\n"))) {
+            on_line(ptr, line_num, ctx);
+            line_num++;
+        }
+        free(copy);
+    }
+}
+
+void split_line(const char *line, const char *delims, cb_on_token on_token, void* ctx) {
     const char *start = line;
     const char *ptr = start;
     while(*ptr != '\0') {
         if (strchr(delims, *ptr) != NULL) {
-            on_token(start, ptr);
+            on_token(start, ptr, ctx);
             start = ptr + 1;
         }
         ptr++;
     }
-    if (start < ptr) on_token(start, ptr);
+    if (start < ptr) on_token(start, ptr, ctx);
 }
---
+++ utils/timer.c
@@ -0,0 +1,15 @@
+#include "timer.h"
+#include <stdio.h>
+
+void timer_start(time_t* t) {
+    *t = time(NULL);
+}
+
+void time_end(time_t* t, const char *stage) {
+    time_t now = time(NULL);
+    long diff_seconds = now - *t;
+    printf("Elapsed time for %s is [%02DH:%02DM:%02DS] ", stage,
+           (int) (diff_seconds / 3600),
+           (int) ((diff_seconds / 60) % 60),
+           (int) (diff_seconds % 60));
+}
